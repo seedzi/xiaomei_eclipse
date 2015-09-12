8d9c7203639a5cbb182561a2a3b30922ef10a2e2
sunbreak <sunbreak.wang@gmail.com>
2015-09-12 22:39:57 +0800

    专题列表页换用Volley加载

--- src/com/xiaomei/yanyu/activity/TopicListActivity.java
+++ src/com/xiaomei/yanyu/activity/TopicListActivity.java
@@ -6,6 +6,11 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.message.BasicNameValuePair;
 
 import com.android.dx.io.instructions.OneRegisterDecodedInstruction;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
+import com.android.volley.toolbox.StringRequest;
 import com.google.gson.Gson;
 import com.umeng.socialize.net.l;
 import com.xiaomei.yanyu.R;
@@ -15,6 +20,8 @@ import com.xiaomei.yanyu.api.HttpUrlManager;
 import com.xiaomei.yanyu.api.exception.XiaoMeiOtherException;
 import com.xiaomei.yanyu.api.http.HttpApi;
 import com.xiaomei.yanyu.api.http.HttpUtil;
+import com.xiaomei.yanyu.api.util.Constant;
+import com.xiaomei.yanyu.bean.Area;
 import com.xiaomei.yanyu.bean.Topic;
 import com.xiaomei.yanyu.util.IntentUtil;
 import com.xiaomei.yanyu.view.TopicAdapter;
@@ -31,12 +38,14 @@ import android.content.AsyncTaskLoader;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ListView;
 
-public class TopicListActivity extends Activity implements OnRefreshListener, OnLastItemVisibleListener, LoaderCallbacks<Object> {
+public class TopicListActivity extends Activity implements OnRefreshListener, OnLastItemVisibleListener {
 
     public static void startActivity(Activity ac, String title, String url) {
         ac.overridePendingTransition(R.anim.activity_slid_in_from_right, R.anim.activity_slid_out_no_change);
@@ -50,9 +59,9 @@ public class TopicListActivity extends Activity implements OnRefreshListener, On
     private String mUrl;
 
     private PullToRefreshListView mPullView;
-    private TopicAdapter mListAdapter;
 
-    private RecommendGoodsLoader mTopicLoader;
+    private RequestQueue mQueue;
+    private TopicAdapter mListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,14 +74,17 @@ public class TopicListActivity extends Activity implements OnRefreshListener, On
         TitleActionBar titleBar = new TitleActionBar(getActionBar());
         titleBar.setTitle(intent.getStringExtra(IntentUtil.EXTRA_TITLE));
 
+        mQueue = XiaoMeiApplication.getInstance().getQueue();
+
         mPullView = (PullToRefreshListView) findViewById(R.id.list);
         mPullView.setOnRefreshListener(this);
+        mPullView.setOnLastItemVisibleListener(this);
         View emptyView = findViewById(R.id.empty);
         mPullView.setEmptyView(emptyView);
         emptyView.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                mTopicLoader.forceLoad();
+                onRefresh(mPullView);
             }
         });
 
@@ -81,69 +93,60 @@ public class TopicListActivity extends Activity implements OnRefreshListener, On
         listView.setAdapter(mListAdapter);
         listView.setOnItemClickListener(new TopicAdapter.TopicItemClickListener());
 
-        mTopicLoader = (RecommendGoodsLoader) getLoaderManager().initLoader(TOPIC_LOADER, null, this);
+        onRefresh(mPullView);
     }
 
     @Override
     public void onRefresh(PullToRefreshBase refreshView) {
-        mTopicLoader.forceLoad();
+        mQueue.add(new StringRequest(getTopicListUrl(1), mRefreshListener, mRefreshErroListener));
     }
 
     @Override
     public void onLastItemVisible() {
-        // TODO Load more
+        int page = mListAdapter.getCount() / Constant.PERPAGE + 1;
+        mQueue.add(new StringRequest(getTopicListUrl(page), mRefreshMoreListener, mRefreshErroListener));
     }
 
-    @Override
-    public Loader<Object> onCreateLoader(int id, Bundle args) {
-        return new RecommendGoodsLoader(this, mUrl);
+    private String getTopicListUrl(int page) {
+        return Uri.parse(mUrl).buildUpon()
+                .appendQueryParameter(HttpUtil.QUERY_CURPAGE, String.valueOf(page))
+                .appendQueryParameter(HttpUtil.QUERY_PERPAGE, String.valueOf(Constant.PERPAGE))
+                .build().toString();
     }
 
-    @Override
-    public void onLoadFinished(Loader<Object> loader, Object data) {
-        if (data != null) {
-            mListAdapter.clear();
-            mListAdapter.addAll((Topic[]) data);
-        }
-        mPullView.onRefreshComplete();
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Object> loader) {
-        
-    }
-
-    public static class RecommendGoodsLoader extends AsyncTaskLoader<Object> {
-
-        private String url;
-
-        public RecommendGoodsLoader(Context context, String url) {
-            super(context);
-            this.url = url;
-        }
-
+    private Listener<String> mRefreshListener = new Listener<String>() {
         @Override
-        public Object loadInBackground() {
-            HttpApi httpApi = XiaoMeiApplication.getInstance().getApi().getHttpApi();
-            HttpGet httpGet = httpApi.createHttpGet(url,
-                    new BasicNameValuePair(HttpUtil.QUERY_CURPAGE, "1"),
-                    new BasicNameValuePair(HttpUtil.QUERY_PERPAGE, "10"));
-            try {
-                BizResult result = httpApi.doHttpRequestResult(httpGet);
-                if (result.isSuccess()) {
-                    return Topic.newGson().fromJson(result.getMessage(), Topic[].class);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (XiaoMeiOtherException e) {
-                e.printStackTrace();
+        public void onResponse(String response) {
+            Gson gson = new Gson();
+            BizResult res = gson.fromJson(response, BizResult.class);
+            if (res.isSuccess()) {
+                Topic[] topics = Topic.newGson().fromJson(res.getMessage(), Topic[].class);
+                mListAdapter.clear();
+                mListAdapter.addAll(topics);
             }
-            return null;
+            mPullView.onRefreshComplete();
         }
+    };
 
+    private Listener<String> mRefreshMoreListener = new Listener<String>() {
         @Override
-        protected void onStartLoading() {
-            forceLoad();
+        public void onResponse(String response) {
+            Gson gson = new Gson();
+            BizResult res = gson.fromJson(response, BizResult.class);
+            if (res.isSuccess()) {
+                Topic[] topics = Topic.newGson().fromJson(res.getMessage(), Topic[].class);
+                mListAdapter.addAll(topics);
+            }
+            Log.d("sunbreak", "mRefreshMoreListener");
+            mPullView.onRefreshComplete();
         }
-    }
+    };
+
+    private ErrorListener mRefreshErroListener = new ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.d("sunbreak", "mRefreshErroListener");
+            mPullView.onRefreshComplete();
+        }
+    };
 }
